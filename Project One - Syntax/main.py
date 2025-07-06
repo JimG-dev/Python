6f82c02fcbfe42fa7a8bda4d5add9ad5c5ab33b0
James Garner <jim.garner@live.co.uk>
2025-07-06 02:35:31 +0100

    Additional syntax for If/Else and Functions

--- Project One - Syntax/main.py
+++ Project One - Syntax/main.py
@@ -1,8 +1,29 @@
+# Variable Declarations
+
 name = "James"
 age = 36
+location = ''
+
+# Functions
+
+def greet(value, another):
+    print(value, another)
+
+greet(12, 45)
+
+# Literals
 
 print(f"Hello {name}, you are {age} years old.")
 
-location = "Doncaster"
 
 print (f"{location}")
+
+# If/Else
+
+
+if location :
+    print(f'{location}')
+
+else :
+    print('None')
+
